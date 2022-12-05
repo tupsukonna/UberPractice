e89fc7c6cbfd670d3bb22da07948370e32a87f1f
TupsuKonna <46424503+tupsukonna@users.noreply.github.com>
2022-12-05 02:14:25 +0200

    new files

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreens from './screens/MapScreens';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name='MapScreens'
+                component={MapScreens}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ screens/MapScreens.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import Map from '../components/Map';
+import MapView from 'react-native-maps';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+
+
+const MapScreens = () => {
+  const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <TouchableOpacity 
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-08 z-50 p-3 rounded-full shadow-lg`}
+      >
+          <Icon name="menu" />
+      </TouchableOpacity> 
+        
+
+      <View style={tw`h-1/2`}>
+        <Map/>
+      </View>
+
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
+    </View>
+  )
+}
+
+export default MapScreens
+
+const styles = StyleSheet.create({})
